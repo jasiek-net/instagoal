d585f65ebf94d59e417bcd679ccc309a7aff1572
Jan Horubała <jan.horubala@zwolnienizteorii.pl>
2020-09-24 17:40:32 +0200

    Add tailwind config setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
+  purge: ['./components/*.vue', './views/*.vue'],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        default: '2rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+      },
+    },
     extend: {},
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+  },
+  corePlugins: {
+    outline: false,
   },
   variants: {},
   plugins: [],
