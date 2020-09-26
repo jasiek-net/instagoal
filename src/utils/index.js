52beb58ec7ffaea5aca0752621cc93ec22f1c185
Jan Horubała <jan.horubala@zwolnienizteorii.pl>
2020-09-26 12:47:35 +0200

    Add API utils

---
+++ src/utils/index.js
@@ -0,0 +1,3 @@
+export const API = 'https://sheets.googleapis.com/v4/spreadsheets/';
+export const KEY = 'P2tleT1BSXphU3lBdGdHakF6M1ZrM3dGSm1vUlBZdXVSQ0V3UlpwZUZ5MGc=';
+export const REG = '/spreadsheets/d/([a-zA-Z0-9-_]+)';
